31a1272127a55e77eac775fe80fc4555dd808de1
David Dovel <davey5555@gmail.com>
2019-03-07 21:54:33 -0600

    Make tictactoe game alternate players

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -2,24 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-	render() {
-		return (
-			<button
-				className="square"
-				onClick={() => this.props.onClick()}
-			>
-				{this.props.value}
-			</button>
-		);
-	}
+function Square(props) {
+	return (
+		<button className="square" onClick={props.onClick}>
+			{props.value}
+		</button>
+	)
 }
 
 class Board extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			squares: Array(9).fill(null)
+			squares: Array(9).fill(null),
+			isXNext: true,
 		}
 	}
 
@@ -34,12 +30,19 @@ class Board extends React.Component {
 
 	handleClick(i) {
 		const squares = this.state.squares.slice();
-		squares[i] = 'X';
-		this.setState({squares: squares})
+		squares[i] = this.getCurrentPlayer();
+		this.setState({
+			squares: squares,
+			isXNext: !this.state.isXNext,
+		});
+	}
+
+	getCurrentPlayer() {
+		return this.state.isXNext ? 'X' : 'O';
 	}
 
 	render() {
-		const status = 'Next player: X';
+		const status = 'Next player: ' + this.getCurrentPlayer();
 
 		return (
 			<div>
